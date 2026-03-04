06213249a7e314c9b7bd05d8ce72bb83ecd3162e
Emerick <pacaudemerick@gmail.com>
2026-03-04 12:41:25 +0100

    Correction serveur

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
-const api = require('./api');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/api', api);
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
