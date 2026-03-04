7f1661eafa253f4b1710b59a64ca81b8e7d5c563
Emerick <pacaudemerick@gmail.com>
2026-03-04 13:58:07 +0100

    Implémantation de l'API

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const api = require('./api');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', api);
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
